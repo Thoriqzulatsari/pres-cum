8cb3b718224e8872ed2410d7826d1688307b3e74
Thoriqzulatsari <thoriqatsari@gmail.com>
2025-06-17 15:11:45 +0700

    config update 3

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'u287442801_presdorm');
+define('DB_NAME', 'presdorm');
 
 // Attempt to connect to MySQL database
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
